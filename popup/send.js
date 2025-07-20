dd9db37f0a6404bf253398bfe4816f68aa540a2d
Alex McColm <mccolmalex@gmail.com>
2025-07-20 12:11:54 -0700

    fix indentation

--- popup/send.js
+++ popup/send.js
@@ -1,13 +1,20 @@
 const shiftToStr = (shift) => {
-    return "From " + shift.shift.start + " to " + shift.shift.end + " on " + shift.date;
+  return "From " + shift.shift.start + " to " + shift.shift.end + " on " + shift.date;
 };
 
 browser.runtime.sendMessage({ from: "popup" }).then((response) => {
-    const shifts = response.data;
-    document.getElementById('shiftNumber').textContent = shifts?.length ?? "No message yet.";
-    shifts.forEach(element => {
-        let li = document.createElement('li');
-        li.textContent = shiftToStr(element);
-        document.getElementById('shiftList').appendChild(li);
-    });
+  const shifts = response.data;
+  document.getElementById('shiftNumber').textContent = shifts?.length ?? "No message yet.";
+  shifts.forEach(element => {
+    let li = document.createElement('li');
+    li.textContent = shiftToStr(element);
+    document.getElementById('shiftList').appendChild(li);
+  });
 });
+
+/**
+* "Send Shifts" button handler. 
+*/
+const sendShifts = () => {
+  console.log("TODO: Send shifts to google API");
+};
