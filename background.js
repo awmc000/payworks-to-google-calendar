dcd1c9dc1f1453b82bb48214645aa4aa467e4156
Alex McColm <mccolmalex@gmail.com>
2025-07-20 10:58:20 -0700

    Successfully sending shifts to popup

--- background.js
+++ background.js
@@ -1,11 +1,10 @@
 let latestMessage = null;
-// Listen for message from content script
+
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.from === "content") {
     latestMessage = message.data;
-    // no sendResponse needed, don't return true
   } else if (message.from === "popup") {
     sendResponse({ data: latestMessage });
-    return true; // async response — but actually sent here immediately
+    return true;
   }
 });
--- popup/send.js
+++ popup/send.js
@@ -1,5 +1,15 @@
+const shiftToStr = (shift) => {
+    return "From " + shift.shift.start + " to " + shift.shift.end + " on " + shift.date;
+};
+
 browser.runtime.sendMessage({ from: "popup" }).then((response) => {
     const shifts = response.data;
-    alert(shifts);
-    document.getElementById('shiftNumber').textContent = shifts?.payworksShifts.length ?? "No message yet.";
+    console.log('getting shifts');
+    console.log(shifts);
+    document.getElementById('shiftNumber').textContent = shifts?.length ?? "No message yet.";
+    shifts.forEach(element => {
+        let li = document.createElement('li');
+        li.textContent = shiftToStr(element);
+        document.getElementById('shiftList').appendChild(li);
+    });
 });
