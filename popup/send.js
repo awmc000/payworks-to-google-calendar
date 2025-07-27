b9595f4c7dae61f4a345c5e0bf46d2d88a40fa68
Alex McColm <mccolmalex@gmail.com>
2025-07-26 22:11:57 -0700

    Remove OAuth code will not use; remove genai policy (empty); improve popup code

--- popup/send.js
+++ popup/send.js
@@ -4,8 +4,13 @@
  * Primary popup script
  */
 
-
-function generateGoogleCalendarLinkLocal({
+/**
+ * Creates a GC event creating link for the event described.
+ * @param {Date} startDateTime 
+ * @param {Date} endDateTime 
+ * @returns A string which is a valid Google Calendar event creating link
+ */
+function generateGcLink({
   title = 'Work',
   startDateTime, // Date object or ISO string
   endDateTime,   // Date object or ISO string
@@ -38,21 +43,34 @@ function generateGoogleCalendarLinkLocal({
   return `${base}&${params.toString()}`;
 }
 
+/**
+ * Wrapper for generateGcLink() tuned to the specifics of a shift object.
+ * @param {*} shift Shift object
+ * @returns Valid GC event creation link
+ */
 const shiftToEventLink = (shift) => {
+
+  // startDate and endDate pass constructors strings of the form "2025-07-05 16:00"
   let startDate = new Date(shift.date + " " + shift.shift.start);
   let endDate = new Date(shift.date + " " + shift.shift.end);
   let detail = shift.shift.description;
-  return generateGoogleCalendarLinkLocal({
-    title: "Work: " + detail, 
+
+  return generateGcLink({
+    title: "Work: " + shift.shift.description, 
     startDateTime: startDate, 
-    endDateTime: endDate}
-  );
+    endDateTime: endDate,
+    details: detail  
+  });
 };
 
 const shiftToStr = (shift) => {
   return shift.shift.startOriginal + " - " + shift.shift.endOriginal + " on " + shift.date.substr(6);
 };
 
+/**
+ * Send a message to the background script to request the shifts object.
+ * Then construct its representation on the DOM.
+ */
 browser.runtime.sendMessage({ from: "popup" }).then((response) => {
   const shifts = response.data;
   document.getElementById('shiftNumber').textContent = shifts?.length ?? "No message yet.";
